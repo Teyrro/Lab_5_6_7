218c0642a1d36a8bbfc5ce11be360096509a2f7a
Илья Харченко <kharchenko_ilia@mail.ru>
2022-03-28 01:00:29 +0700

    Minor improvements

--- Lab_5/Main.cpp
+++ Lab_5/Main.cpp
@@ -1,6 +1,6 @@
-//#define LAGRANGE
+#define LAGRANGE
 //#define AITKEN
-#define NEWTON
+//#define NEWTON
 
 
 #include "Interpolation.h"
@@ -11,28 +11,25 @@ int main() {
 	SetConsoleOutputCP(1251);
 #ifdef LAGRANGE
 	Interpolation f("StorageOfData.txt", 1.69);
-
 	f.P();
-	f.OutputData();
-	for (double i(0); i <= 4.2; i += 0.1) {
-		f.SetNewValue(i);
-		f.P();
-		f.OutputDataValue();
-	}
+	
 #endif
 
 #ifdef AITKEN
 	Aitken_Interpolation f("StorageOfData.txt", 2.56);
-	f.OutputData();
 	f.FindAnswer();
+
 #endif
 
 #ifdef NEWTON
 	Newton f("StorageOfData.txt", 1.69);
-	f.OutputData();
 	f.NewtonAnswer(true);
 #endif
-	
-
+	f.OutputData();
+	for (double i(0); i <= f.storageOfData[f.storageOfData.size() - 1].first; i += 0.1) {
+		f.SetNewValue(i);
+		f.P();
+		f.OutputDataValue();
+	}
 	
 }
